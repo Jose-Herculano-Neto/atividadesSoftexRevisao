e3537f1920dd9221f3653b3ce04891b8929b6a0f
Jose-Herculano-Neto <herculano.neto@hotmail.com>
2023-08-16 17:25:14 -0300

    3 a 30

---
+++ exerciciosRevisao/tresATrinta.js
@@ -0,0 +1,10 @@
+//imprima os múltiplos de 3 de 0 a 30 utilizando um loop for.
+
+export function tresATrinta(){
+    for(let i = 0; i <= 30; i++){
+        if(i % 3 == 0){
+            console.log(i);
+        }
+    }
+
+}
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { fibon } from "./exerciciosRevisao/fibonacci.js";
 import { mult } from "./exerciciosRevisao/mult.js";
 import { pares } from "./exerciciosRevisao/pares.js";
 import { soma } from "./exerciciosRevisao/soma.js";
+import { tresATrinta } from "./exerciciosRevisao/tresATrinta.js";
 
 
 contador();
@@ -10,3 +11,4 @@ soma();
 pares();
 mult();
 fibon();
+tresATrinta();
